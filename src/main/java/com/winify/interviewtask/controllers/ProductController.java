48f16f61792257e12d10a6b64e0ee49dfc27b976
Artemie Vovc <artemie.vovc@codefactorygroup.com>
2019-09-26 18:23:10 +0300

    Thymeleaf

--- src/main/java/com/winify/interviewtask/controllers/ProductController.java
+++ src/main/java/com/winify/interviewtask/controllers/ProductController.java
@@ -1,24 +1,25 @@
 package com.winify.interviewtask.controllers;
 
-import com.winify.interviewtask.domain.Product;
 import com.winify.interviewtask.sevices.ProductService;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class ProductController {
 
-  Logger logger = LoggerFactory.getLogger(ProductController.class);
+  private Logger logger = LoggerFactory.getLogger(ProductController.class);
 
   @Autowired private ProductService productService;
 
-  @GetMapping(produces = "application/json")
-  public Iterable<Product> getAll(HttpServletRequest request) {
+  @RequestMapping
+  public String getAll(HttpServletRequest request, Model model) {
     logger.info("Method: {} Path: {}", request.getMethod(), request.getContextPath());
-    return productService.all();
+    model.addAttribute("products", productService.all());
+    return "homepage";
   }
 }
